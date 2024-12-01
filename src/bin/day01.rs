0c79615bea78989acc935ab9c74f605c4645c4d4
ChaseParate <me@chasehark.com>
2024-11-30 23:17:01 -0700

    Refactor Day 1 solution

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,28 +1,40 @@
-fn main() {
-    let input = include_str!("day01_input.txt");
-
-    let mut list1 = Vec::new();
-    let mut list2 = Vec::new();
+fn parse_input(input: &str) -> Vec<(usize, usize)> {
+    let mut lists = Vec::new();
 
     for line in input.lines() {
         let (left, right) = line.split_once("   ").unwrap();
-        list1.push(left.parse::<u32>().unwrap());
-        list2.push(right.parse::<u32>().unwrap());
+        lists.push((left.parse().unwrap(), right.parse().unwrap()));
     }
 
-    list1.sort_unstable();
-    list2.sort_unstable();
+    lists
+}
 
-    let answer = list1
-        .iter()
-        .zip(&list2)
-        .map(|(left, right)| left.abs_diff(*right))
-        .sum::<u32>();
-    println!("Part 1: {}", answer);
+fn part_one(parsed_input: &[(usize, usize)]) -> usize {
+    let (mut left_list, mut right_list): (Vec<_>, Vec<_>) = parsed_input.iter().copied().unzip();
 
-    let mut answer2 = 0;
-    for item in list1 {
-        answer2 += item * list2.iter().filter(|x| **x == item).count() as u32;
-    }
-    println!("Part 2: {}", answer2);
+    left_list.sort_unstable();
+    right_list.sort_unstable();
+
+    left_list
+        .into_iter()
+        .zip(right_list)
+        .map(|(left, right)| left.abs_diff(right))
+        .sum()
+}
+
+fn part_two(parsed_input: &[(usize, usize)]) -> usize {
+    let (left_list, right_list): (Vec<_>, Vec<_>) = parsed_input.iter().copied().unzip();
+
+    left_list
+        .into_iter()
+        .map(|left| left * right_list.iter().filter(|right| left == **right).count())
+        .sum()
+}
+
+fn main() {
+    let input = include_str!("day01_input.txt");
+    let parsed_input = parse_input(input);
+
+    println!("Part 1: {}", part_one(&parsed_input));
+    println!("Part 2: {}", part_two(&parsed_input));
 }
