44e8cb7c47e4796b467237b54b69a0c28d84a693
ChaseParate <me@chasehark.com>
2024-12-03 10:50:16 -0700

    Remove unnecessary reference

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -52,12 +52,12 @@ mod tests {
     #[test]
     fn part_one_example() {
         let input = r"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
-        assert_eq!(161, part_one(&input));
+        assert_eq!(161, part_one(input));
     }
 
     #[test]
     fn part_two_example() {
         let input = r"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
-        assert_eq!(48, part_two(&input));
+        assert_eq!(48, part_two(input));
     }
 }
